63978175dcae4650f5a8e8eade8737bac56e99ea
Greg Haskins <greg@manetu.com>
2022-08-17 17:46:04 -0400

    Update metadata
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject io.github.manetu/gitlab-api "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :description "A babashka compatible library for accessing the gitlab API "
+  :url "https://github.com/manetu/gitlab-api"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [babashka/babashka.curl "0.1.2"]
                  [environ "1.2.0"]
